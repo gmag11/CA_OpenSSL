b6632cf3b1d4ab75c25728b50983e9595c1fbf4c
Germán Martín <github@gmartin.net>
2021-04-03 20:52:14 +0200

    Add clean script

---
+++ 0. ca_clean.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+rm -r intermediate/certs
+rm -r intermediate/crl
+rm -r intermediate/csr
+rm -r intermediate/private
+rm intermediate/crlnumber*
+rm intermediate/index*
+rm intermediate/serial*
+
+rm -r certs
+rm -r crl
+rm -r csr
+rm -r private
+rm index*
+rm serial*
